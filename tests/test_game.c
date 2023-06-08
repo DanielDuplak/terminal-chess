18580bee8211a69371c69c7b7c1d76f3d8e9b670
Daniel Duplak <daniel.duplak@gmail.com>
2023-06-08 20:28:12 +0200

    added game over test

--- tests/test_game.c
+++ tests/test_game.c
@@ -17,8 +17,20 @@ TEST test_destroy_game()
     PASS();
 }
 
+TEST test_is_game_over()
+{
+    struct piece* board[BOARD_ROWS][BOARD_COLS];
+
+    create_board_fen(board, "r5kr/ppp2Q2/1b1p3p/1N4p1/1P6/5R1P/P1q2P2/2B2RK1");
+    ASSERT(is_game_over(board, false));
+    destroy_game(board);
+
+    PASS();
+}
+
 
 SUITE(test_game)
 {
     RUN_TEST(test_destroy_game);
+    RUN_TEST(test_is_game_over);
 }
